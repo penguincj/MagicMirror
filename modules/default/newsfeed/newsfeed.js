50a095a617e3cddf88b241241295c9e83938f688
刘传军 <liuchuanjun@meituan.com>
2019-01-05 10:23:07 +0800

    for news interval

--- modules/default/newsfeed/newsfeed.js
+++ modules/default/newsfeed/newsfeed.js
@@ -27,7 +27,7 @@ Module.register("newsfeed",{
 		lengthDescription: 400,
 		hideLoading: false,
 		reloadInterval: 5 * 60 * 1000, // every 5 minutes
-		updateInterval: 100 * 1000,
+		updateInterval: 5 * 1000,
 		animationSpeed: 2.5 * 1000,
 		maxNewsItems: 0, // 0 for unlimited
 		ignoreOldItems: false,
